0a26aafdecf71a1d1e63390a97d4797616d666ee
Teodor Grigor <open-source@teogor.dev>
2026-08-23 05:28:02 +0300

    feat(react): export initial component set

---
+++ packages/react/src/index.ts
@@ -0,0 +1,3 @@
+export { Surface, type SurfaceProps } from './Surface.js';
+export { Button, type ButtonProps, type ButtonVariant } from './Button.js';
+export { Card, type CardProps } from './Card.js';
