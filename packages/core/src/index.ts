28ef9054a4dba6e86ef028e71b67bef5e7ad6dc3
Teodor Grigor <open-source@teogor.dev>
2026-08-23 05:26:48 +0300

    feat(core): expose framework-agnostic primitives

---
+++ packages/core/src/index.ts
@@ -0,0 +1,17 @@
+export {
+  createSurfaceState,
+  getSurfaceClassNames,
+  type SurfaceElevation,
+  type SurfaceKind,
+  type SurfaceOptions,
+  type SurfaceState,
+} from './surface.js';
+
+export {
+  getInteractionClassName,
+  resolveInteractionState,
+  type InteractionOptions,
+  type InteractionState,
+} from './interaction.js';
+
+export { cx, getDisabledProps, type AriaDisabledProps, type ClassValue } from './primitives.js';
