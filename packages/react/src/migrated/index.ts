3c11413e88c784a37dec67d0c1ccd792b1f88f23
Teodor Grigor <open-source@teogor.dev>
2026-08-23 05:40:09 +0300

    feat(react): add migrated component barrel

---
+++ packages/react/src/migrated/index.ts
@@ -0,0 +1,7 @@
+export * from './actions.js';
+export * from './containment.js';
+export * from './feedback.js';
+export * from './inputs.js';
+export * from './navigation.js';
+export * from './overlays.js';
+export * from './web-adapted.js';
